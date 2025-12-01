b899e982e67c7064add40f8bf4d59daf072a9d8c
MacAbrams <fishbock@gmail.com>
2025-12-01 15:25:27 -0500

    multiply loop optimized

--- asm/multiply_p2_loop.asm
+++ asm/multiply_p2_loop.asm
@@ -1,15 +1,10 @@
 START:
-	LOADI R0, #0x05
-	LOADI R1, #0x00 ;count
-	LUI R1, #0x00
+	LOADI R0, #0x05 ;constant to multiply
+	LOADI R1, #0x9 ;number of times to multiply
 	LOADI R2, #0x01 ;constant 1
-	LOADI R3, #0x8
 L1:
-	SHFT R0, R0, R2
-	STORE R0, [R1 + #0x00]
-	ADD R1, R1, R2 ; increment count
-	SUB R4, R1, R3
+	SHFT R3, R0, R1
+	SUB R1, R1, R2
+	STORE R3, [R1 + #0x0000]
 	BNE L1
 	HALT
-	
-	
