86745af20047053652cb9d0b965cb3054e019495
Julien Chabert <fauxcheur597@gmail.com>
2025-12-01 23:11:53 -0500

    finished divide_p2.asm

---
+++ asm/divide_p2.asm
@@ -0,0 +1,25 @@
+START:
+    LOADI R0, #0xC0
+    LUI R0, #0x01
+    LOADI R1, #0x1
+    LUI R1, #0x80
+
+    SHFT R2, R0, R1
+    ADDI R1, R1, #0x1
+
+    SHFT R3, R0, R1
+    ADDI R1, R1, #0x1
+
+    SHFT R4, R0, R1
+    ADDI R1, R1, #0x1
+
+    SHFT R5, R0, R1
+    ADDI R1, R1, #0x1
+
+    SHFT R6, R0, R1
+    ADDI R1, R1, #0x1
+
+    SHFT R7, R0, R1
+
+
+    HALT
