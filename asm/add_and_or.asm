92ea328b6595980b05b2c2215a5b37dbcfb76305
MacAbrams <fishbock@gmail.com>
2025-12-02 15:12:01 -0500

    I fixed your formatting for you

--- asm/add_and_or.asm
+++ asm/add_and_or.asm
@@ -1,1 +1,13 @@
-START:    LOADI R0, #0xAA    LOADI R1, #0x55    AND R3 , R1, R0    BEQ SAFE    LOADI R2, #0x0    ;we need to use 3 loadi, and the solution I found were either this, or    ;having the loadi with the other 3, and have    ; AND R2,R2,R2 (which is a bit silly)    HALTSAFE:    OR R2, R1, R0    HALT
+START:
+	LOADI R0, #0xAA
+    LOADI R1, #0x55
+    AND R3 , R1, R0
+    BEQ SAFE
+    LOADI R2, #0x0
+    ;we need to use 3 loadi, and the solution I found were either this, or
+    ;having the loadi with the other 3, and have
+    ; AND R2,R2,R2 (which is a bit silly)
+    HALT
+SAFE:
+    OR R2, R1, R0
+    HALT
