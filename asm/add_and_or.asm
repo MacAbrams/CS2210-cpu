e183700a290813f47761370f5d76c1290eeeecc1
Fauche-r <fauxcheur597@gmail.com>
2025-12-03 13:10:47 -0500

    Improved

--- asm/add_and_or.asm
+++ asm/add_and_or.asm
@@ -1,19 +1,12 @@
 START:
     LOADI R0, #0xAA
     LOADI R1, #0x55
-
+    LOADI R2, #0x0
 
     AND R3 , R1, R0
-    BEQ SAFE
-    LOADI R2, #0x0
-    ;we need to use 3 loadi, and the solution I found were either this, or
-    ;having the loadi with the other 3, and have
-    ; AND R2,R2,R2 (which is a bit silly)
-    B DONE
+    BNE DONE
 
 SAFE:
     OR R2, R1, R0
 DONE:
     HALT
-
-
